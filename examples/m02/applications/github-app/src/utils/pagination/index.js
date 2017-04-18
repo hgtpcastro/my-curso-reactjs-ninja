0c32ce52b8f54522c944b5b792cab4dbf38292e5
Hugo Castro Costa <hg.tp.castro@gmail.com>
2017-04-18 15:49:24 -0300

    M2#A52 - Paginação - implementação inicial

--- examples/m02/applications/github-app/src/utils/pagination/index.js
+++ examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const pagination = ({ total, activePage }) => {
+  // let pages = []
+  // for (let i = 0; i < total; i++) {
+  //   pages.push(i + 1)
+  // }
+
+  // return pages
+
+  // ES5
+  // return Array.apply(null, { length: total }).map((_, i) => i + 1)
+
+  // ES6
+  return Array.from({ length: total }, (_, i) => i + 1)
+}
+
+export default pagination
